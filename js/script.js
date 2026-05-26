9fb94b2d8850cc15984be54b52c462f3c1ffa99e
alejandroVargas08 <manuelvargasnoriega@gmail.com>
2026-05-26 15:13:24 -0500

    Es la pagina completa de ALERTA, se le hizo algunos cambios a REPORTE, pero el resto esta igual

--- js/script.js
+++ js/script.js
@@ -30,47 +30,45 @@ const boton = document.querySelector('.select-boton');
 const opciones = document.querySelectorAll('.select-menu li');
 const texto = document.querySelector('.select-texto');
 
-
-boton.addEventListener('click', () => {
-    select.classList.toggle('abierto');
-});
-
-opciones.forEach(opcion => {
-    opcion.addEventListener('click', () => {
-        texto.textContent = opcion.textContent;
-        select.classList.remove('abierto');
+if (boton && select) {
+    boton.addEventListener('click', () => {
+        select.classList.toggle('abierto');
     });
-});
 
-document.addEventListener('click', (e) => {
-    if (!select.contains(e.target)) {
-        select.classList.remove('abierto');
-    }
-});
-
+    opciones.forEach(opcion => {
+        opcion.addEventListener('click', () => {
+            texto.textContent = opcion.textContent;
+            select.classList.remove('abierto');
+        });
+    });
+}
 
 //Sobre los tipos de reporte
 const selectFormato = document.querySelector('.select-formato');
 const botonFormato = document.querySelector('.boton-formato');
-const opcionesFormato = selectFormato.querySelectorAll('.select-menu2 li');
-const textoFormato = selectFormato.querySelector('.select-texto2');
 
-botonFormato.addEventListener('click', () => {
-    selectFormato.classList.toggle('abierto');
-});
+if (botonFormato && selectFormato) {
+    const opcionesFormato = selectFormato.querySelectorAll('.select-menu2 li');
+    const textoFormato = selectFormato.querySelector('.select-texto2');
 
-opcionesFormato.forEach(opcion => {
-    opcion.addEventListener('click', () => {
-        textoFormato.textContent = opcion.textContent;
-        selectFormato.classList.remove('abierto');
+    botonFormato.addEventListener('click', () => {
+        selectFormato.classList.toggle('abierto');
     });
-});
+
+    opcionesFormato.forEach(opcion => {
+        opcion.addEventListener('click', () => {
+            textoFormato.textContent = opcion.textContent;
+            selectFormato.classList.remove('abierto');
+        });
+    });
+}
+
 document.addEventListener('click', (e) => {
-    if (!select.contains(e.target)) select.classList.remove('abierto');           
-    if (!selectFormato.contains(e.target)) selectFormato.classList.remove('abierto'); 
+    if (select && !select.contains(e.target)) select.classList.remove('abierto');
+    if (selectFormato && !selectFormato.contains(e.target)) selectFormato.classList.remove('abierto');
 });
 
-//Para que seleccione donde estoy ubiicado 
+//Para que seleccione donde estoy ubicado
 const linksMenu = document.querySelectorAll('.lista-menu a');
 
 linksMenu.forEach(link => {
@@ -79,4 +77,18 @@ linksMenu.forEach(link => {
     } else {
         link.parentElement.classList.remove('activo');
     }
+});
+
+//Botones activas y resueltas
+const botonesOpcion = document.querySelectorAll('.opcion');
+const contenidosOpcion = document.querySelectorAll('.opcion-contenido');
+
+botonesOpcion.forEach(opcion => {
+    opcion.addEventListener('click', () => {
+        botonesOpcion.forEach(o => o.classList.remove('activo'));
+        contenidosOpcion.forEach(c => c.classList.remove('activo'));
+
+        opcion.classList.add('activo');
+        document.getElementById(opcion.dataset.tab).classList.add('activo');
+    });
 });
