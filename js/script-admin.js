f2b4b544ca466f6d10422079aabe1a76c2dbd0a1
alejandroVargas08 <manuelvargasnoriega@gmail.com>
2026-05-29 22:12:33 -0500

    Realice el gestion de Usuarios

--- js/script-admin.js
+++ js/script-admin.js
@@ -49,3 +49,40 @@ tarjetas.forEach(tarjeta => {
         tarjeta.style.boxShadow = 'none';
     });
 });
+
+//Parte de Gestion de usuario
+const selects = document.querySelectorAll('.select-gestion');
+
+selects.forEach(select => {
+    select.addEventListener('change', function() {
+        if(this.value !== this.options[0].value) {
+            this.classList.add('select-activo');
+        } else {
+            this.classList.remove('select-activo');
+        }
+    });
+});
+//Despliegue de los menu
+const menusDesplegables = document.querySelectorAll('.menu-desplegable');
+
+menusDesplegables.forEach(menu => {
+    menu.addEventListener('click', function(e) {
+        e.stopPropagation();
+        this.classList.toggle('abierto');
+    });
+
+    const items = menu.querySelectorAll('.desplegable-item');
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            const seleccionado = menu.querySelector('.desplegable-seleccionado span');
+            seleccionado.textContent = this.textContent;
+            items.forEach(i => i.classList.remove('desplegable-activo'));
+            this.classList.add('desplegable-activo');
+            menu.classList.remove('abierto');
+        });
+    });
+});
+
+document.addEventListener('click', () => {
+    menusDesplegables.forEach(m => m.classList.remove('abierto'));
+});
